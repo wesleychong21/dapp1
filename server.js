a0fada19abae2fccc9f19ab9c75ecdd636abf5f0
Wesleychong21 <myerp.my@gmail.com>
2021-09-20 18:46:47 +0800

    Posting with post

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require ('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+
+app.use(bodyParser.json());
 
 app.post('/', (req, res)=>{
     var email = req.body.email;
-    var email = req.body.amount;
+    var amount = req.body.amount;
 
     res.send({"amount": amount, "email": email});
 })
 
-/*
+
 app.get('/', (req, res)=>{
     res.send("HELLO WEB 2.0");
 })
-*/
+
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
